d92a375d52df4f1d39d1fc6125173712f8c63209
Cedric Geissmann <cedric.geissmann@gmail.com>
2023-10-18 16:09:31 +0200

    Slightly better styles.

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 export function print(args) {
-  var element = document.querySelector("#output")
+  var element = document.querySelector("#output-content")
   var oldContent = element.textContent
   element.textContent = `${oldContent}\n> ${args}`
 }
@@ -17,4 +17,37 @@ export function $(callback) {
   const value = read()
   const result = callback(value)
   print(result)
+}
+
+export function increaseFontSize() {
+  const cssRoot = document.querySelector(":root")
+  const rootStyles = getComputedStyle(cssRoot)
+  let currentFontSize = rootStyles.getPropertyValue("--output-font-size")
+  currentFontSize = currentFontSize.split("pt")[0]
+  cssRoot.style.setProperty(
+    "--output-font-size",
+    `${parseInt(currentFontSize) + 2}pt`,
+  )
+}
+
+export function decreaseFontSize() {
+  const cssRoot = document.querySelector(":root")
+  const rootStyles = getComputedStyle(cssRoot)
+  let currentFontSize = rootStyles.getPropertyValue("--output-font-size")
+  currentFontSize = currentFontSize.split("pt")[0]
+  cssRoot.style.setProperty(
+    "--output-font-size",
+    `${parseInt(currentFontSize) - 2}pt`,
+  )
+}
+
+export function decreaseOutputHeight() {
+  const cssRoot = document.querySelector(":root")
+  const rootStyles = getComputedStyle(cssRoot)
+  let currentFontSize = rootStyles.getPropertyValue("--output-height")
+  currentFontSize = currentFontSize.split("px")[0]
+  cssRoot.style.setProperty(
+    "--output-height",
+    `${parseInt(currentFontSize) - 20}px`,
+  )
 }
